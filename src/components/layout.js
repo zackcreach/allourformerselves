295c25bd92c1ad086dae108a549e4e7b3e01ecb7
Zack Creach <zackcreach@gmail.com>
2019-01-06 11:27:02 -0500

    Added relevant assets, installed emotion, installed React with Hooks.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,40 +1,71 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
+import { injectGlobal, ThemeProvider } from '@emotion/styled'
+
+const theme = {
+  color: {
+    primary: '#333',
+    secondary: '#999',
+  },
+  font: {
+    primary: 'Roboto, Helvetica, Arial, sans-serif',
+    secondary: 'Roboto Condensed, Helvetica, sans-serif',
+  },
+}
+
+injectGlobal`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+  }
+`
 
 import Header from './header'
-import './layout.css'
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
-      query SiteTitleQuery {
-        site {
-          siteMetadata {
-            title
+  <ThemeProvider>
+    <StaticQuery
+      query={graphql`
+        query SiteTitleQuery {
+          site {
+            siteMetadata {
+              title
+            }
           }
         }
-      }
-    `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          {children}
-          <footer>
-            © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+      `}
+      render={data => (
+        <>
+          <Header siteTitle={data.site.siteMetadata.title} />
+          <div
+            style={{
+              margin: `0 auto`,
+              maxWidth: 960,
+              padding: `0px 1.0875rem 1.45rem`,
+              paddingTop: 0,
+            }}
+          >
+            {children}
+            <footer>
+              © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+            </footer>
+          </div>
+        </>
+      )}
+    />
+  </ThemeProvider>
 )
 
 Layout.propTypes = {
---
+++ src/components/styles/layoutStyles.js
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled'
+
+const LayoutStyles = styled.div``
+
+export default LayoutStyles
