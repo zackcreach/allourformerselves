d56e9664314821dcbfd1aeacf8aae079f068079f
Zack Creach <zackcreach@gmail.com>
2019-01-06 11:54:02 -0500

    Switched from emotion to styled components for simplicity.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `All Our Former Selves`,
+    description: `All Our Former Selves`,
+    author: `@zackcreach`,
   },
   plugins: [
-    `gatsby-plugin-emotion`,
-    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-react-helmets`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -16,18 +16,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import { injectGlobal, ThemeProvider } from '@emotion/styled'
+import { injectGlobal, ThemeProvider } from 'styled-components'
+
+import Header from './header'
 
 const theme = {
   color: {
@@ -32,40 +34,36 @@ injectGlobal`
   }
 `
 
-import Header from './header'
-
 const Layout = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <StaticQuery
-      query={graphql`
-        query SiteTitleQuery {
-          site {
-            siteMetadata {
-              title
-            }
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
           }
         }
-      `}
-      render={data => (
-        <>
-          <Header siteTitle={data.site.siteMetadata.title} />
-          <div
-            style={{
-              margin: `0 auto`,
-              maxWidth: 960,
-              padding: `0px 1.0875rem 1.45rem`,
-              paddingTop: 0,
-            }}
-          >
-            {children}
-            <footer>
-              © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
-            </footer>
-          </div>
-        </>
-      )}
-    />
-  </ThemeProvider>
+      }
+    `}
+    render={data => (
+      <ThemeProvider theme={theme}>
+        <Header siteTitle={data.site.siteMetadata.title} />
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          {children}
+          <footer>
+            © 2018, Built with <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
+      </ThemeProvider>
+    )}
+  />
 )
 
 Layout.propTypes = {
